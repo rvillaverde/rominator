d53e20387e0390e1b476bc70a838c997a5b5e50e
Romina Villaverde <romina.villaverde@gmail.com>
2013-04-19 11:44:59 -0300

    Fixed tab issues

--- app/models/console_asker.rb
+++ app/models/console_asker.rb
@@ -1,15 +1,15 @@
 class ConsoleAsker
 
-	def self.ask(question, response_type, validate, message)
+  def self.ask(question, response_type, validate, message)
     answer = ask_question(question, response_type)
     if (validate)
-	    until valid_response?(response_type, answer) do
-	      puts message
-	      answer = ask_question(question, response_type)
-	    end
-	  end
+      until valid_response?(response_type, answer) do
+        puts message
+        answer = ask_question(question, response_type)
+      end
+    end
     extract_response(response_type, answer)
-	end
+  end
 
 private
 
@@ -21,32 +21,32 @@ private
   def self.valid_response?(response_type, response)
     case response_type
     when :string
-    	return !response.empty?
+      return !response.empty?
     when :integer
-    	return !response.empty?
+      return !response.empty?
     when :boolean
       return response.downcase.start_with?('s') || response.downcase.start_with?('n')
     end
   end
 
-	def self.extract_response(response_type, response)
-		case response_type
-		when :string
+  def self.extract_response(response_type, response)
+    case response_type
+    when :string
       return response unless response.empty?
-		when :integer
+    when :integer
       return response.to_i
-		when :boolean
+    when :boolean
       return response.downcase.start_with?('s')
-		end
-	end
+    end
+  end
 
   def self.response_type_leyend(response_type)
-  	case response_type
-  	when :integer
-  		return "[Ingrese un numero]"
-  	when :boolean
-  		return "[Contestar Si/No]"
-  	end
+    case response_type
+    when :integer
+      return "[Ingrese un numero]"
+    when :boolean
+      return "[Contestar Si/No]"
+    end
   end
 
 end
